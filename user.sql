18529feecc160cb8f8c053b3ca37f28dc9964ec7
藤本大輝 <d_fujimoto@ic-com.co.jp>
2016-05-27 11:06:09 +0900

    users table

--- user.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `loginid` varchar(20) DEFAULT NULL,
-  `password` varchar(255) NOT NULL,
-  `name` varchar(10) DEFAULT NULL,
-  `corporation` int(11) DEFAULT NULL,
-  `position` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)

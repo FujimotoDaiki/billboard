ebf53b4023493fae1a7358703f445d6da42fd341
藤本大輝 <d_fujimoto@ic-com.co.jp>
2016-05-27 10:08:29 +0900

    comment table

---
+++ comment.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `comment` (
+
+  `id` int(11) NOT NULL,
+
+  `text` varchar(45) DEFAULT NULL,
+
+  `date_and_time` varchar(45) DEFAULT NULL,
+
+  `name` varchar(45) DEFAULT NULL,
+
+   PRIMARY KEY (`id`)

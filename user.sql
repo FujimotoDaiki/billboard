b2f71b42dd8a1327e0421901a72ec7fd6e94b98b
藤本大輝 <d_fujimoto@ic-com.co.jp>
2016-05-27 10:38:23 +0900

    user table

--- user.sql
+++ user.sql
@@ -1,12 +1,8 @@
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-
-  `password` varchar(45) NOT NULL,
-
-  `name` varchar(45) DEFAULT NULL,
-
-  `corporation` varchar(45) DEFAULT NULL,
-
-  `position` varchar(45) DEFAULT NULL,
-
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `loginid` varchar(20) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(10) DEFAULT NULL,
+  `corporation` int(11) DEFAULT NULL,
+  `position` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
